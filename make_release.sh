2d469d1285d35bd696c52ff5a8f2aa1ca2b7fb14
Brecht Sanders <brecht@sanders.org>
2024-01-10 11:55:26 +0100

    Add make_release.sh

---
+++ make_release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# determine version number
+SCALEDNUM_VERSION_MAJOR=$(sed -ne "s/^#define\s*SCALEDNUM_VERSION_MAJOR\s*\(.*\)/\1/p" include/scalednum.h)
+SCALEDNUM_VERSION_MINOR=$(sed -ne "s/^#define\s*SCALEDNUM_VERSION_MINOR\s*\(.*\)/\1/p" include/scalednum.h)
+SCALEDNUM_VERSION_MICRO=$(sed -ne "s/^#define\s*SCALEDNUM_VERSION_MICRO\s*\(.*\)/\1/p" include/scalednum.h)
+
+# determine target platform
+SCALEDNUM_PLATFORM_TRIPLET=$(${CC:-gcc} -v 2>&1|sed -ne "s/^Target: \(.*\)$/\1/p")
+SCALEDNUM_ARCH=$(echo $SCALEDNUM_PLATFORM_TRIPLET|sed -e "s/^\([^-]*\)-.*$/\1/")
+SCALEDNUM_PLATFORM=$(echo $SCALEDNUM_PLATFORM_TRIPLET|sed -e "s/^.*-\([^-]*\)$/\1/")
+if echo $SCALEDNUM_PLATFORM|grep -q "^\(mingw\|windows\)"; then
+ SCALEDNUM_PLATFORM=windows
+fi
+
+BUILDDIR=$(realpath $(dirname $0))/build.$SCALEDNUM_ARCH.$SCALEDNUM_PLATFORM
+# configure, build and test
+cmake -Wno-dev -GNinja -DCMAKE_INSTALL_PREFIX:PATH=$BUILDDIR/install -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED:BOOL=ON -DBUILD_STATIC:BOOL=ON -DBUILD_DEMO:BOOL=ON -S. -B$BUILDDIR/build &&
+ninja -C$BUILDDIR/build install/strip &&
+pushd $BUILDDIR/install &&
+tar cfJ ../../scalednum-$SCALEDNUM_VERSION_MAJOR.$SCALEDNUM_VERSION_MINOR.$SCALEDNUM_VERSION_MICRO-$SCALEDNUM_PLATFORM-$SCALEDNUM_ARCH.tar.xz * &&
+zip -r9 ../../scalednum-$SCALEDNUM_VERSION_MAJOR.$SCALEDNUM_VERSION_MINOR.$SCALEDNUM_VERSION_MICRO-$SCALEDNUM_PLATFORM-$SCALEDNUM_ARCH.zip * &&
+popd &&
+rm -rf $BUILDDIR &&
+echo Done
